43881e7da00e0d83afee777ca572711af12a8e66
rociobessio <rocibessio@gmail.com>
2023-12-18 00:12:08 -0300

    Agregando templates socio

---
+++ socio/manejador-empleados.php
@@ -0,0 +1,11 @@
+<!-- Con php incluyo el header -->
+<?php include("partials/menu.php")?>
+
+<!-- Cuerpo principal -->
+<main class="main-content">
+    <div class="wrapper">
+        <h1>Panel Empleados</h1>
+    </div>
+</main>
+<!-- Con php incluyo el footer -->
+<?php include("partials/footer.php")?>
--- socio/partials/menu.php
+++ socio/partials/menu.php
@@ -14,17 +14,17 @@
         <header>
             <div class="menu">
                 <div class="wrapper">
-                <div class="menu en-linea">
-                    <ul>
-                        <li><a href="../index.html">Home</a></li>
-                        <li><a href="./templates/categories.html">Empleados</a></li>
-                        <li><a href="./templates/foods.html">Pedidos</a></li>
-                        <li><a href="./templates/foods.html">Productos</a></li>
-                        <li><a href="./templates/foods.html">Mesas</a></li>
-                    </ul>
-                </div>
-                    <!-- <h1>Panel de Control Socios</h1> -->
-    
+                    <div class="menu en-linea">
+                        <ul>
+                            <li><a href="./index.php">Home</a></li>
+                            <li><a href="./manejador-socio.php">Administrador</a></li>
+                            <li><a href="./manejador-categorias.php">Categorias</a></li>
+                            <li><a href="./manejador-productos.php">Productos</a></li>
+                            <li><a href="./manejador-mesas.php">Mesas</a></li>
+                            <li><a href="./manejador-empleados.php">Empleados</a></li>
+                            <li><a href="./manejador-pedidos.php">Pedidos</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </header>
